2c7bf7656114eb2619439f56296608d0503cb24d
samc0123 <schernov1999@gmail.com>
2023-03-24 18:32:35 -0400

    Completed knn model predictor, removed others. Transactions returning as one df w/ cats

--- shared_functions/manual_trans_category.py
+++ shared_functions/manual_trans_category.py
@@ -4,20 +4,10 @@ from collections import Counter
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split as tss
 from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
-from sklearn.naive_bayes import GaussianNB
 
-def manual_categorize_transactions(raw_transaction_data:pd.DataFrame) -> pd.DataFrame:
-    ''' Manually categorize [raw_transaction_data] for ML training data '''
-    transaction_cateogry_dict = {
-        "Entertainment":"AMC",
-        "Grocery":"Aldi,Trader Joe's,Wegmans,NC700",
-        "Online":"Amazon,eBay,",
-        "Wholesale":"Walmart,Target"
-    }
-    print(transaction_cateogry_dict.get('Grocery'))
+
+
 
 def count_most_frequent_words(data:pd.DataFrame,df_col_to_countFreqWords:str,countWords:int):
     '''Count the most frequent words in given column of a dataframe'''
@@ -81,121 +71,3 @@ def transaction_knn_model(training_dataset:pd.DataFrame,actual_dataset:pd.DataFr
     actual_dataset.reset_index(inplace=True,drop=True)
     return actual_dataset
 
-def transaction_LSVC_model(training_dataset:pd.DataFrame,actual_dataset:pd.DataFrame) :
-    '''Run knn algo on training_dataset to try to predict values in actual_dataset'''
-    # Create encoder labels for each categorical dataset
-    lblEncodeX = LabelEncoder()
-    lblEncodey = LabelEncoder()
-    lblEncodeact = LabelEncoder()
-    
-    # Split training data for model based on word frequency in transactions
-    X = lblEncodeX.fit_transform(training_dataset["nameTrans"]).reshape(-1,1)  
-    y = lblEncodey.fit_transform(training_dataset["category"]).reshape(-1,1).ravel() # needed for proper shaping to the model
-    x_actual = lblEncodeact.fit_transform(actual_dataset["Payee"]).reshape(-1,1) # actual dataset to predict on
-    X_train,X_test,y_train,y_test = tss(X,y,test_size=0.2) # random state - matches the training set breakdown in\
-        # test, stratify ensures breakdown across all columns 
-
-    # Instatiate linear SVC 
-    lsvc = LinearSVC(verbose=0, max_iter=2000)
-    lsvc.fit(X_train,y_train)
-
-    print(lsvc.score(X_test,y_test))
-
-def transaction_linearReg_model(training_dataset:pd.DataFrame,actual_dataset:pd.DataFrame):
-    '''Run linear reg algo to predict the bank transaction categories'''
-    # Create encoder labels for each categorical dataset
-    lblEncodeX = LabelEncoder()
-    lblEncodey = LabelEncoder()
-    lblEncodeact = LabelEncoder()
-    
-    # Split training data for model based on word frequency in transactions
-    X = lblEncodeX.fit_transform(training_dataset["nameTrans"]).reshape(-1,1)  
-    y = lblEncodey.fit_transform(training_dataset["category"]).reshape(-1,1) # needed for proper shaping to the model
-    x_actual = lblEncodeact.fit_transform(actual_dataset["Payee"]).reshape(-1,1) # actual dataset to predict on
-    X_train,X_test,y_train,y_test = tss(X,y,test_size=0.2) # random state - matches the training set breakdown in\
-        # test, stratify ensures breakdown across all columns 
-
-    # Scale variables 
-    scaler_x = preprocessing.StandardScaler().fit(X_train)
-    X_scaled = np.asarray(scaler_x.transform(X_train)).reshape(-1,1)
-
-    scaler_y = preprocessing.StandardScaler().fit(y_train)
-    y_scaled = np.asarray(scaler_y.transform(y_train))
-
-    # Instantiate the linear reg model 
-    linear_reg = LinearRegression()
-    linear_reg.fit(X_scaled,y_scaled)
-
-    # Scale outputs 
-    X_test_scaler = preprocessing.StandardScaler().fit(X_test)
-    X_test_scaled = np.asarray(X_test_scaler.transform(X_test)).reshape(-1,1)
-
-    y_test_scaler = preprocessing.StandardScaler().fit(y_test)
-    y_test_scaled = np.asarray(y_test_scaler.transform(y_test))
-
-
-    # See model performance 
-    print(linear_reg.score(X_test_scaled,y_test_scaled))
-
-def transaction_bayes_model(training_dataset:pd.DataFrame,actual_dataset:pd.DataFrame):
-     # Create encoder labels for each categorical dataset
-    lblEncodeX = LabelEncoder()
-    lblEncodey = LabelEncoder()
-    lblEncodeact = LabelEncoder()
-    ordEncoderX = preprocessing.OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value=-1)
-    ordEncoderXAct = preprocessing.OrdinalEncoder(handle_unknown='use_encoded_value',unknown_value=-1)
-    
-    # Split training data for model based on word frequency in transactions
-    bank_trans_data ={
-        'training_trans': training_dataset["nameTrans"],
-        'trans_to_predict': actual_dataset["Payee"]
-    }
-    bank_trans_array = np.concatenate((training_dataset["nameTrans"],actual_dataset["Payee"]),axis=None)
-    bank_trans_df = pd.DataFrame(bank_trans_data)
-    # Encode transactions from banking statements 
-    ordEncoderX.fit(bank_trans_array.reshape(-1,1))
-    bank_trans_df[['training_trans']] = ordEncoderX.transform(bank_trans_df[['training_trans']])
-    bank_trans_df[['trans_to_predict']] = ordEncoderX.transform(bank_trans_df[['trans_to_predict']])
-   
-    
-    X = np.asarray(bank_trans_df['training_trans']).reshape(-1,1)
-    y = lblEncodey.fit_transform(training_dataset["category"]).reshape(-1,1).ravel() # needed for proper shaping to the model
-    x_actual = np.asarray(bank_trans_df['trans_to_predict'])
-    x_actual = np.delete(x_actual,np.where(x_actual == -1)).reshape(-1,1)
-    X_train,X_test,y_train,y_test = tss(X,y,test_size=0.2) # random state - matches the training set breakdown in\
-        # test, stratify ensures breakdown across all columns 
-
-    """  # Scale variables train
-    scaler_x = preprocessing.StandardScaler().fit(X_train)
-    X_scaled = np.asarray(scaler_x.transform(X_train)).reshape(-1,1)
-
-    scaler_y = preprocessing.StandardScaler().fit(y_train)
-    y_scaled = np.asarray(scaler_y.transform(y_train)) 
-    
-    # Sepeate ordinal encoders 
-    ordEncoderX.fit(bank_trans_df[['training_trans']])
-    bank_trans_df[['training_trans']] = ordEncoderX.transform(bank_trans_df[['training_trans']])
-
-    ordEncoderXAct.fit(bank_trans_df[['trans_to_predict']])
-    bank_trans_df[['trans_to_predict']] = ordEncoderXAct.transform(bank_trans_df[['trans_to_predict']])
-    """
-
-    # Instantiate the bayes model
-    gnb = GaussianNB()
-    gnb.fit(X_train,y_train)
-    #print(gnb.score(X_test,y_test))
-    #print(ordEncoderX.inverse_transform(X_test))
-    #print(lblEncodey.inverse_transform(y_test))
-     # Fit model with data 
-    vals = gnb.predict(X=x_actual)
-    print(ordEncoderX.inverse_transform(x_actual))
-    print(lblEncodey.inverse_transform(vals))
-
-    """ # Scale test
-    X_test_scaler = preprocessing.StandardScaler().fit(X_test)
-    X_test_scaled = np.asarray(X_test_scaler.transform(X_test)).reshape(-1,1)
-
-    y_test_scaler = preprocessing.StandardScaler().fit(y_test)
-    y_test_scaled = np.asarray(y_test_scaler.transform(y_test)) """
-
-    print(gnb.score(X_test,y_test))
