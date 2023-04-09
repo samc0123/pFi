6fb169b231a20da61af36249d7ef9934019cd423
samc0123 <schernov1999@gmail.com>
2023-04-08 23:25:38 -0400

    Finalized manipulation for master_dataframe, aggregator next

--- financePredictor.py
+++ financePredictor.py
@@ -2,27 +2,38 @@ import pandas as pd
 import numpy as np
 import glob
 import os 
+from tkinter import *
+from tkinter import ttk
 
 from shared_functions import df_manipulations,manual_trans_category
 
+## TODO: 
+    # create tkinter front-end to allow users to select path for training\
+        # and test files from their respective locations
+    
 
 def main():
     # Import all csv files into dataframe
     print("test entering main")
-    path_test = r'/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/trans_sheets_test' # use your path
-    path_training_file = r'/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/trans_sheets_train/manual_training_trans_cats_ident/predictedCats_knn.csv'
+    path_master_transFile = r'/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/Master_transaction_file/master_transactions_manReview.csv'
+    path_newBankStatements = r'/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/new_transSheets'
+    testing_file_path = r'/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/knnPredictions.csv'
 
-    transaction_df = df_manipulations.read_transactions_to_mainFrame(main_path=path_test,fType='.csv')
+    transaction_df = df_manipulations.read_transactions_to_mainFrame(main_path=path_newBankStatements,fType='.csv')
     print(transaction_df)
 
     columns_indep_dep = {
-        'training_independent':'nameTrans',
+        'training_independent':'merchant',
         'training_dependent':'category',
         'actual_independent':'merchant'
     }
-    training_data = pd.read_csv(filepath_or_buffer=path_training_file)
+    training_data = pd.read_csv(filepath_or_buffer=path_master_transFile)
     df_trans_w_categories = manual_trans_category.transaction_knn_model(training_dataset=training_data,actual_dataset=transaction_df,columns_indep_dep = columns_indep_dep)
-    df_trans_w_categories.to_csv(path_or_buf='/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/knnPredictions.csv')
+
+    # Write new predictions to the master_transactions_manReview.csv file 
+    df_trans_w_categories.to_csv(path_or_buf=path_master_transFile,mode='a',\
+        index=False,header=False)
+    
 
     
 
--- shared_functions/df_manipulations.py
+++ shared_functions/df_manipulations.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import glob
 import os 
+from datetime import datetime as dt 
+import csv
+import sys
 
 def read_transactions_to_mainFrame(main_path:str,fType:str) -> pd.DataFrame:
     '''Returns a concatenated dataframe of '''
@@ -17,15 +20,30 @@ def read_transactions_to_mainFrame(main_path:str,fType:str) -> pd.DataFrame:
             ,'Amount']),
         'BofA_deposits': np.asarray(['Date','Description','Amount','Running Bal.'])
     }
+
     # Specify columns needed for processing 
     cols_to_keep_from_bank_statemts = {
         'Chase_cc':np.asarray(['Transaction Date','Description','Amount']),
         'BofA_cc':np.asarray(['Posted Date','Payee','Amount']),
         'BofA_deposits':np.asarray(['Date','Description','Amount'])
     }
+
     # Specify final dataframe column names 
     final_cols = ['datePosted','merchant','amount']
 
+ 
+
+    # Specify latest dates ran from the statements to avoid duplication
+    latest_dates_added_master_file = {
+    }
+    fPathdates = main_path+'/transDates/lastest_statement_dates.csv'
+    with open(file=fPathdates,mode='r') as ld:
+        readCSV_latestStatementDates = csv.DictReader(ld)
+        for col in readCSV_latestStatementDates:
+            latest_dates_added_master_file = col
+    
+    ld.close()
+    print(latest_dates_added_master_file)
 
     # Import files into list for concatenation
     li = [] # list of final dataframes 
@@ -34,16 +52,43 @@ def read_transactions_to_mainFrame(main_path:str,fType:str) -> pd.DataFrame:
         # Read current file into temporary dataframe
         temp_df = pd.read_csv(filepath_or_buffer=file,index_col=None, header=0)
         
-        # Find statement type
+        # Find statement type by checking headers of current statemnt and comparing with dict
         statement_type = [k for k,v in \
             cols_downloaded_from_bank_statement.items()\
-                if np.array_equiv(a1=np.asarray(temp_df.columns.values),a2=v)]
+                if np.array_equiv(a1=np.asarray(temp_df.columns.values),a2=v)].pop()
         
         # Keep the appropriate columns and add dataframe to list for concat
-        temp_df = temp_df[cols_to_keep_from_bank_statemts[statement_type.pop()]]
+            # by popping the statement type and then using this val in the dict
+            # to return the appropriate columns to keep from temp_df
+        temp_df = temp_df[cols_to_keep_from_bank_statemts[statement_type]]
+        # Reassign column names
         temp_df.columns = final_cols 
+
+        # Find the earliest and latest dates, alert of potential duplicate statement
+        earliest_date = temp_df['datePosted'].min()
+        latest_date = temp_df['datePosted'].max()
+        print(latest_dates_added_master_file[statement_type])
+        if pd.to_datetime(earliest_date) < pd.to_datetime(latest_dates_added_master_file\
+            [statement_type]):
+            print('Warning: Potentially duplicate statement. Please review\
+                master transaction file manually.')
+            input('Press Enter to continue...')
+        # Modify the current statements latest date as the current latest date
+        if latest_dates_added_master_file[statement_type] < latest_date:
+            latest_dates_added_master_file[statement_type] < latest_date
+
+        # Add current df to be concated for final list of transactions
         li.append(temp_df)
 
+    # Write latest dates to tracking file
+    namesFields = list(latest_dates_added_master_file.keys())
+    with open(file=fPathdates,mode='w') as editDates:
+        datesCSVwriter = csv.DictWriter(f=editDates,fieldnames=namesFields)
+        datesCSVwriter.writeheader()
+        datesCSVwriter.writerow(latest_dates_added_master_file)
+    
+    # Concat & format frame to return
     transaction_frame = pd.concat(objs=li,axis=0,ignore_index=True)
+    # Replace commas in thousands #'s
     transaction_frame['amount'] = transaction_frame['amount'].replace(',','',regex=True)
     return(transaction_frame)
