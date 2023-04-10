35baf77c3f930c6c4bd795a8319d98d3b111de2c
samc0123 <schernov1999@gmail.com>
2023-04-09 21:43:21 -0400

    Added bar graph plotting mechanism to code, need to work more on Tkinter piece

--- predictionGrouping.py
+++ predictionGrouping.py
@@ -7,16 +7,28 @@ import os
     # categories and process them into a dataframe
 
 
-""" for new function, after doing manual review on predeictions
+def group_trans_month_cat(path_masterTrans:str) -> pd.DataFrame:
+    '''Group transaction dataframe by month and category'''
+
+    # Fetch the dataframe
+    df_transactions = pd.read_csv(filepath_or_buffer=path_masterTrans)
+    
+
     # Group by category, then calculate total spending of each category
-    group_df_trans = df_trans_w_categories.groupby(by=['category','year','month']).sum()
+    group_df_trans = df_transactions.groupby(by=['category','year','month']).sum()
     group_df_trans['amount'] = group_df_trans['amount'].round(2) # round money to 2 places
-    group_df_trans.to_csv() """
+    group_df_trans.sort_values(by=['year','month'],axis='index',ascending=True,inplace=True)
+    
+    return group_df_trans
 
 
 def main():
-    pass
+    # Transaction file location
+    path_transFolder = r'/Users/samchernov/Desktop/Personal/Financials/Transaction_Export/pFi/Master_transaction_file/'
 
+    # Group by month and category
+    grouped_df = group_trans_month_cat(path_masterTrans=path_transFolder+'master_transactions_manReview.csv')
+    grouped_df.to_csv(path_or_buf=path_transFolder+'/grouped_master_trans.csv')
 if __name__ == "__main__":
-    print('this is a test')
+    print('Now categorizing transactions')
     main()
